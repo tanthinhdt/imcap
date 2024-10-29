6a1da9c2afbbf98f74e42b511a0ae8fefe021089
tanthinhdt <tanthinhdt24@gmail.com>
2024-10-29 16:02:08 +0700

    feat(tests): add test for flickr30k datamodule

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -1,13 +1,10 @@
-from pathlib import Path
-
 import pytest
-import torch
-
-from src.data.mnist_datamodule import MNISTDataModule
+from pathlib import Path
+from src.data import Flickr30kDataModule
 
 
 @pytest.mark.parametrize("batch_size", [32, 128])
-def test_mnist_datamodule(batch_size: int) -> None:
+def test_flickr30k_datamodule(batch_size: int) -> None:
     """Tests `MNISTDataModule` to verify that it can be downloaded correctly, that the necessary
     attributes were created (e.g., the dataloader objects), and that dtypes and batch sizes
     correctly match.
@@ -15,24 +12,58 @@ def test_mnist_datamodule(batch_size: int) -> None:
     :param batch_size: Batch size of the data to be loaded by the dataloader.
     """
     data_dir = "data/"
+    data_dir = Path(data_dir)
+    assert data_dir.exists(), \
+        f"{data_dir} does not exist. Make sure to download the dataset."
 
-    dm = MNISTDataModule(data_dir=data_dir, batch_size=batch_size)
-    dm.prepare_data()
+    dataset_dir = data_dir / "flickr30k"
+    assert dataset_dir.exists(), \
+        f"{dataset_dir} does not exist. Make sure to download the dataset."
+    assert Path(dataset_dir, "results.csv").exists(), \
+        "Metadata file does not exist. Make sure to include the metadata file."
+    assert Path(dataset_dir, "flickr30k_images").exists(), \
+        "Image directory does not exist. Make sure to include the image directory."
 
-    assert not dm.data_train and not dm.data_val and not dm.data_test
-    assert Path(data_dir, "MNIST").exists()
-    assert Path(data_dir, "MNIST", "raw").exists()
+    dm = Flickr30kDataModule(data_dir=dataset_dir, batch_size=batch_size)
+    assert not dm.dataset, \
+        "Dataset should not be loaded before calling `setup`."
 
     dm.setup()
-    assert dm.data_train and dm.data_val and dm.data_test
-    assert dm.train_dataloader() and dm.val_dataloader() and dm.test_dataloader()
+    assert "train" in dm.dataset and "test" in dm.dataset, \
+        "Dataset should be split into training and testing sets."
+    if "val" in dm.dataset:
+        num_datapoints = len(dm.dataset["train"]) + len(dm.dataset["val"]) + len(dm.dataset["test"])
+    else:
+        num_datapoints = len(dm.dataset["train"]) + len(dm.dataset["test"])
+    assert num_datapoints == 158_915, \
+        "The dataset should contain 158,915 datapoints."
 
-    num_datapoints = len(dm.data_train) + len(dm.data_val) + len(dm.data_test)
-    assert num_datapoints == 70_000
-
+    assert dm.train_dataloader() and dm.val_dataloader() and dm.test_dataloader(), \
+        "Dataloaders should be created after calling `setup`."
     batch = next(iter(dm.train_dataloader()))
-    x, y = batch
-    assert len(x) == batch_size
-    assert len(y) == batch_size
-    assert x.dtype == torch.float32
-    assert y.dtype == torch.int64
+
+    assert batch["images"].shape == (batch_size, 3, 224, 224), \
+        (
+            f"Images shape mismatch. Expected {(batch_size, 3, 224, 224)}, "
+            f"but got {batch['images'].shape}."
+        )
+    assert batch["input_ids"].shape == (batch_size, 128), \
+        (
+            f"Input IDs shape mismatch. Expected {(batch_size, 128)}, "
+            f"but got {batch['input_ids'].shape}."
+        )
+    assert batch["attention_mask"].shape == (batch_size, 128), \
+        (
+            f"Attention Mask shape mismatch. Expected {(batch_size, 128)}, "
+            f"but got {batch['input_ids'].shape}."
+        )
+    assert batch["token_type_ids"].shape == (batch_size, 128), \
+        (
+            f"Token Type IDs shape mismatch. Expected {(batch_size, 128)}, "
+            f"but got {batch['token_type_ids'].shape}."
+        )
+    assert batch["comment_number"].shape == (batch_size,), \
+        (
+            f"Comment Number shape mismatch. Expected {(batch_size,)}, "
+            f"but got {batch['comment_number'].shape}."
+        )
